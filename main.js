b72eb14cac582e0e15aa1611bc0be89e9763aca8
Simon Adam <adam.simon@smartbits.hu>
2025-08-07 17:27:09 +0200

    Enable scraping functionality in main.js and remove unused save.json file

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { scrape } from './scrape.js'
 import { runConcurrentRequests } from './checker2.js'
 import fs from 'fs'
 ;(async () => {
-    // await scrape()
+    await scrape()
     const domainsJSON = JSON.parse(fs.readFileSync('result.json', 'utf8'))
     const result = await runConcurrentRequests(domainsJSON, 5000)
     console.log(result)
